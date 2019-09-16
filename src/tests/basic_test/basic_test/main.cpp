9631d0188f4e13fb2897aae015037f7e9045e4fe
VgTajdd <aduranddiaz@gmail.com>
2019-09-16 07:55:30 -0500

    Add Application class to control time

--- src/tests/basic_test/basic_test/main.cpp
+++ src/tests/basic_test/basic_test/main.cpp
@@ -1,10 +1,52 @@
-#include "../../../chess/ChessBoard.h"
-#include <iostream>
+#include "../../../game/ChessGame.h"
+#include <thread>
+#include <chrono>
+
+class Application
+{
+public:
+	Application();
+	~Application();
+public:
+	void start();
+private:
+	bool m_finished;
+	std::chrono::steady_clock::time_point m_lastTime;
+	ChessGame* m_game;
+};
+
+Application::Application():
+	m_finished( false ),
+	m_game( nullptr )
+{
+	m_game = new ChessGame();
+	m_game->start();
+}
+
+Application::~Application()
+{
+	delete m_game;
+}
+
+void Application::start()
+{
+	m_lastTime = std::chrono::steady_clock::now();
+	while ( !m_finished )
+	{
+		std::chrono::milliseconds delta( 16 );
+		std::this_thread::sleep_for( delta );
+		if ( m_game != nullptr )
+		{
+			std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+			int dt = int( std::chrono::duration_cast<std::chrono::milliseconds>( now - m_lastTime ).count() );
+			m_game->update( dt );
+			m_lastTime = now;
+		}
+	}
+}
 
 int main()
 {
-	ChessBoard chessboard;
-	chessboard.getState();
-	std::cin.get();
-	return 0;
+	Application app;
+	app.start();
 }
